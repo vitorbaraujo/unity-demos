84cb028b41f7d3da5c759f644ecee2f5fbb4673a
Vitor Barbosa <vitor.araujo@tfgco.com>
2019-07-01 10:19:36 -0300

    Start TowerfallArrow demo
    - Move character horizontally

---
+++ Assets/TowerfallArrow/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int xSpeed;
+    public int ySpeed;
+
+    private Rigidbody2D rb;
+    private float moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * xSpeed, rb.velocity.y);
+    }
+}
